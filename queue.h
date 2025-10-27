c33c510c994423ae964c042b362d589023208b82
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2025-10-27 23:02:44 +0200

    Added code for queue files

--- queue.h
+++ queue.h
@@ -4,5 +4,21 @@
 
 #ifndef QUEUE_H
 #define QUEUE_H
+#define QUEUE_SIZE 10
+#include "task.h"
 
+typedef struct {
+    TCB_t tcb[QUEUE_SIZE];
+}queue_t;
+
+queue_t * queue_init(void);
+
+//Adds a task to the queue
+void enqueue(queue_t *,const TCB_t);
+
+//Removes and returns a task from the queue
+TCB_t dequeue(queue_t *);
+
+//Peeks to the last task from the queue, returning it without remvoing it from the queue
+TCB_t peek(const queue_t *);
 #endif //QUEUE_H
