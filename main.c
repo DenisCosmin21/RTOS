7f3beb4eb0c7c8500537f8d70111ea2129f84dff
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2025-11-22 00:46:54 +0200

    Added main.h

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "queue.h"
+#include "task.h"
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    TCB_t task1 = newTask(LOW, 2);
+    TCB_t task2 = newTask(HIGH, 2);
+    TCB_t task3 = newTask(LOW, 3);
+    TCB_t task;
+    queue_t queue;
+    queue_init(&queue);
+    task = peek(&queue);
+    enqueue(&queue, task1);
+    enqueue(&queue, task2);
+    task = peek(&queue);
+    task = dequeue(&queue);
+    enqueue(&queue, task3);
+    task = dequeue(&queue);
+    task = dequeue(&queue);
 }
