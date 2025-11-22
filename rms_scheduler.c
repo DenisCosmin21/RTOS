b0ec793a643a074c7cf1e95a71abb08ebd7b83b4
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2025-11-22 05:43:07 +0200

    Made scheduler more efficient, and ready for runtime task adding. Removed hard double operations, and added specific rms bound to each number of tasks becouse of small amount of max tasks

--- rms_scheduler.c
+++ rms_scheduler.c
@@ -1,42 +1,121 @@
 ﻿#include "rms_scheduler.h"
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+
+#include "priority_queue.h"
+#include "task_queues.h"
+
+const static float RMS_BOUNDS[10] = {
+    1.0000,   // n=1
+    0.8284,   // n=2
+    0.7798,   // n=3
+    0.7568,   // n=4
+    0.7435,   // n=5
+    0.7348,   // n=6
+    0.7286,   // n=7
+    0.7241,   // n=8
+    0.7206,   // n=9
+    0.7177,   // n=10
+};
+
+static int periods_per_priority[4];
+
+static float utilization = 0;
+
+static task_queues_t task_queues;
+
+static int new_task_condition(const TCB_t* task, const TCB_t *task2) {
+    return task->period > task2->period;
+}
+
+static int return_task_from_template(const TCB_t *task) {
+    return 1;
+}
 
 void rms_task_templates_init(rms_task_templates_t* templates) {
-    if (!templates) return;
-    templates->num_tasks = 0;
-    for (int i = 0; i < MAX_TASKS; i++)
-        templates->task_templates[i] = createEmptyTask();  //SE POT INITIALIZA COZILE DIRECT SI SCAPI DE STRUCTURA TEMPLATES
+    p_init(&templates->tasks);
 }
 
-bool rms_task_templates_add(rms_task_templates_t* templates, TCB_t task) {
-    if (!templates) return false;
-    if (templates->num_tasks >= MAX_TASKS) return false;
-    if (task.period <= 0 || task.executionTime <= 0) return false;
-    templates->task_templates[templates->num_tasks++] = task;     //TASK UL ARE DEJA O PRIORITATE CAND IL CREEZI SI DOAR VOOM DA ADD TASK
-    return true;
+short rms_task_templates_add(rms_task_templates_t* templates,const TCB_t task) {
+    p_enqueue(&templates->tasks, task, new_task_condition);
+    utilization += (float)(task.execution_time) / (float)(task.period);
+#ifdef DEBUG
+    printf("Utilization = %f\n", utilization);
+#endif
+
+    return 1;
 }
 
-double calculate_utilization(const rms_task_templates_t* templates) {
-    if (!templates || templates->num_tasks == 0) return 0.0;
-    double U = 0.0;
-    for (int i = 0; i < templates->num_tasks; i++)
-        U += (double)templates->task_templates[i].executionTime /
-        (double)templates->task_templates[i].period;
-    return U;
+float calculate_rms_bound(int number_of_tasks) {
+    return RMS_BOUNDS[number_of_tasks - 1];
 }
 
-double calculate_rms_bound(int n) {
-    if (n <= 0) return 0.0;
-    return n * (pow(2.0, 1.0 / n) - 1.0);
+short is_schedulable(const rms_task_templates_t* templates) {
+    if(utilization <= 0.69)
+        return 1;
+
+    float rms_bound = calculate_rms_bound(p_get_size(&templates->tasks) - 1);
+#ifdef DEBUG
+    printf("Utilization %f <= Max possible time %f", utilization, rms_bound);
+#endif
+    return utilization <= rms_bound;
 }
 
-bool is_schedulable(const rms_task_templates_t* templates) {
-    if (!templates || templates->num_tasks == 0) return false;
-    return calculate_utilization(templates) <= calculate_rms_bound(templates->num_tasks);
+short start_scheduler(rms_task_templates_t* templates) {
+    if(!is_schedulable(templates) || p_is_empty(&templates->tasks))
+        return 0;
+
+    task_queues_init(&task_queues);
+
+    priority_t current_priority = LOW;
+    int current_period = 0;
+
+    TCB_t task = p_dequeue(&templates->tasks, return_task_from_template);
+    current_period =  task.period;
+    task.priority = current_priority;
+    task_queues_enqueue(&task_queues, task);
+    periods_per_priority[current_priority] = current_period;
+
+    while(!p_is_empty(&templates->tasks)) {
+        task = p_dequeue(&templates->tasks, return_task_from_template);
+        if(task.period != current_period) {
+            current_period = task.period;
+            current_priority--;
+            periods_per_priority[current_priority] = current_period;
+        }
+
+        task.priority = current_priority;
+        task_queues_enqueue(&task_queues, task);
+    }
+
+    #ifdef DEBUG
+        printf("Finished setting up the scheduler\n");
+        print_task_queues(&task_queues);
+    #endif
+    return 1;
 }
 
+TCB_t get_next_task(void) {
+    return task_queues_dequeue(&task_queues);
+}
+
+void wait_task(const TCB_t task) {
+    task_queues_move_to_pending(&task_queues, task);
+}
+
+void release_tasks(void) {
+    while(task_queues_restore_from_pending(&task_queues));
+}
+
+short should_preempt(const TCB_t *task) {
+    return exists_higher_priority_task(&task_queues, task);
+}
+
+void store_back_task(const TCB_t *task) {
+    task_queues_enqueue(&task_queues, *task);
+}
+
+/*
 void rms_task_templates_print(const rms_task_templates_t* templates) {
     if (!templates || templates->num_tasks == 0) {
         printf("Task template set is empty.\n");
@@ -89,3 +168,4 @@ void rms_print_analysis(const rms_task_templates_t* templates) {
 
     printf("====================================\n\n");
 }
+*/
--- rms_scheduler.h
+++ rms_scheduler.h
@@ -2,21 +2,23 @@
 #define RMS_SCHEDULER_H
 
 #include "task.h"
-#include <stdbool.h>
-
-#define MAX_TASKS 10
+#include "priority_queue.h"
 
 typedef struct {
-    TCB_t task_templates[MAX_TASKS];
-    int num_tasks;
+    linear_priority_queue_t tasks;
 } rms_task_templates_t;
 
 void rms_task_templates_init(rms_task_templates_t* templates);
-bool rms_task_templates_add(rms_task_templates_t* templates, TCB_t task);
-double calculate_utilization(const rms_task_templates_t* templates);
-double calculate_rms_bound(int n);
-bool is_schedulable(const rms_task_templates_t* templates);
+short rms_task_templates_add(rms_task_templates_t* templates,const TCB_t task);
+short is_schedulable(const rms_task_templates_t* templates);
+short start_scheduler(rms_task_templates_t* templates);
+TCB_t get_next_task(void);
+void wait_task(const TCB_t task);
+void release_tasks(void);
+short should_preempt(const TCB_t *task);
+void store_back_task(const TCB_t *task);
+/*
 void rms_task_templates_print(const rms_task_templates_t* templates);
 void rms_print_analysis(const rms_task_templates_t* templates);
-
+*/
 #endif
