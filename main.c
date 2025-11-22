92258830170a1a4040ff23927076a815cca9d29f
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2025-11-22 05:44:26 +0200

    Main.c has a simulation of rtos

--- main.c
+++ main.c
@@ -1,36 +1,19 @@
+#define DEBUG
 #include <stdio.h>
-#include "rms_scheduler.h"
-#include "rms_simulator.h"
-#include "task.h"
-#include "task_queues.h"
+
+#include "rtos.h"
 
 int main(void) {
-    printf("=== Rate Monotonic Scheduling (RMS) Simulation ===\n\n");
+#ifdef DEBUG
+    printf("Testing RTOS\n");
+    #endif
+    rtos_task_create(3, 20, "1");
 
-    // Initialize simulator for 10 time units
-    rms_simulator_t simulator;
-    rms_simulator_init(&simulator, 10, true);
+    rtos_task_create(2, 5, "2");
 
-    // Add tasks with hardcoded priorities - one for each priority level
-    TCB_t task1 = newTask("Task1", 2, 4, VERY_HIGH);  // Highest priority
-    task1.nextReleaseTime = 0;
+    rtos_task_create(2, 10, "3");
 
-    TCB_t task2 = newTask("Task2", 4, 4, VERY_HIGH); //SE POT SCOATE PRIORITATILE HARDCODATE DACA FACEM FUNCTIE
-    task2.nextReleaseTime = 0;
+    rtos_start();
 
-    TCB_t task3 = newTask("Task3", 4, 8, MEDIUM);
-    task3.nextReleaseTime = 0;
-
-    TCB_t task4 = newTask("Task4", 3, 10, LOW);  // Lowest priority
-    task4.nextReleaseTime = 0;
-
-    rms_simulator_add_task(&simulator, task1);
-    rms_simulator_add_task(&simulator, task2);
-    rms_simulator_add_task(&simulator, task3);
-    rms_simulator_add_task(&simulator, task4);
-
-    // Run simulation
-    rms_simulator_run(&simulator);
-
-    return 0;
+    simulate_rtos();
 }
