97a747adedb74c9da2da2dd4709c301d13d9a305
Draghici Denis Cosmin <154325453+DenisCosmin21@users.noreply.github.com>
2025-11-23 18:18:04 +0200

    Added new main.c

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #define DEBUG
 #include <stdio.h>
+
+#include "Memory/memory_pool.h"
 #include "RtosApi/rtos.h"
 
 int main(void) {
+    rtos_init();
     rtos_task_create(3, 20, "1");
 
     rtos_task_create(2, 5, "2");
